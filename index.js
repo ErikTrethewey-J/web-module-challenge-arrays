3318c0e2fa983dccd29fe196bf2fc4ca07fcc2d2
ErikTrethewey-J <erikenobi@gmail.com>
2020-10-08 13:10:24 -0700

    Turning in completed project

--- index.js
+++ index.js
@@ -137,9 +137,14 @@ Use the removeFlavorByName function below to do the following:
 
 function removeFlavorByName(arr, string) {
   /*your code here*/
-  for (let i = 0; i < originalFlavors.length; i++) if (arr[i] === string);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === string) {
+      arr.splice(i, 1);
+    }
+  }
+  return arr;
 }
-console.log(removeFlavorByName(originalFlavors, "Vinalla"));
+console.log(removeFlavorByName(originalFlavors, "Vanilla"));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. 
@@ -160,10 +165,17 @@ Use the filterByWord function below to do the following:
     DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/) {
+function filterByWord(arr, string) {
   /*your code here*/
+  const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].includes(string)) {
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
 }
-
+console.log(filterByWord(originalFlavors, "Chocolate"));
 /* 💪💪💪💪💪🧁🍦🍨 STRETCH 🍨🍦🍫💪💪💪💪💪*/
 
 /* STRETCH 1: Write a function that returns the average number of words in an array. You should be able to use this function for any array, but can test with originalFlavors.
